efd1671f5e9fc69a05e9571f8e9ca515dcf5ba9d
Parth Kathane <parth.kathane23@pccoepune.org>
2025-01-24 00:34:10 +0530

    Update HP.js

--- HP.js
+++ HP.js
@@ -1,30 +1,44 @@
 let url = "https://potterapi-fedeperin.vercel.app/en/characters";
-let name1 = document.querySelector("#card1 h5");
-let img1 = document.querySelector("#card1 img");
-let house1 = document.querySelector("#card1 #House");
-let children1 = document.querySelector("#card1 #Children");
-let protrayedBy = document.querySelector("#card1 #ProtrayedBy");
+let cards = document.querySelectorAll(".card");
 
-const getdata = async () =>{
+const getdata = async () => {
     console.log("Fetching data....");
     let response = await fetch(url);  //Here await is imp, boz of asynchro.
     console.log(response);
     console.log("Successful part-1");
-    let data =await response.json()
+    let data = await response.json()
     console.log(data);
     console.log("Successful part-2");
-    // console.log(data[0].image);
-    img1.src = data[0].image;
-    img1.alt = data[0].fullName;
-    // console.log(name1.innerText);
-    name1.innerHTML = data[0].fullName;
-    house1.innerText = `Belongs to ${data[0].hogwartsHouse}`;
-    // console.log(data[0].children);
-    let children_names = data[0].children.join(", ");
-    // console.log(children_names)
-    children1.innerText = `Children.. ${children_names}`;
-    protrayedBy.innerText = `Portrayed by ${data[0].interpretedBy}`
+
+    cards.forEach((card) => {
+        let randomNumber = Math.floor(Math.random() * data.length);
+        // dynamically account for API response size
+        let name = card.querySelector("h5");
+        let img = card.querySelector("img");
+        let house = card.querySelector("#House");
+        let children = card.querySelector("#Children");
+        let protrayedBy = card.querySelector("#ProtrayedBy");
+        let DOB = card.querySelector("#DOB");
+        img.src = data[randomNumber].image;
+        img.alt = data[randomNumber].fullName;
+        DOB.innerText = `- ${data[randomNumber].birthdate}`;
+        name.innerHTML = data[randomNumber].fullName;
+        house.innerText = `Belongs to ${data[randomNumber].hogwartsHouse}`;
+
+        if(data[randomNumber].children && data[randomNumber].children.length > 0){
+            let children_names = data[randomNumber].children.join(", ");
+            children.innerText = `Children.. ${children_names}`;
+        }
+        else{
+            children.innerText = "Data of this Character's Child is not Available";
+        }
+        protrayedBy.innerText = `Portrayed by ${data[randomNumber].interpretedBy}`
+    })
 }
 
+let shuffle = document.querySelector(".btn");
+shuffle.addEventListener("click",()=>{
+    return getdata();
+});
 
 window.onload = getdata;
