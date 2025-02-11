9f1d4ba6a198b81bc16249737ad957fcbe8a2264
Parth Kathane <parth.kathane23@pccoepune.org>
2025-02-11 11:25:54 +0000

    New Branch Created and Updated.

--- HP.js
+++ HP.js
@@ -1,39 +1,40 @@
 let url = "https://potterapi-fedeperin.vercel.app/en/characters";
-let cards = document.querySelectorAll(".card");
+
+let Cars = document.querySelectorAll(".Car");
 
 const getdata = async () => {
-    console.log("Fetching data....");
+    console.log("Fetching data");
     let response = await fetch(url);  //Here await is imp, boz of asynchro.
     console.log(response);
     console.log("Successful part-1");
     let data = await response.json()
     console.log(data);
     console.log("Successful part-2");
-
-    cards.forEach((card) => {
+    Cars.forEach((Car) => {
         let randomNumber = Math.floor(Math.random() * data.length);
-        // dynamically account for API response size
-        let name = card.querySelector("h5");
-        let img = card.querySelector("img");
-        let house = card.querySelector("#House");
-        let children = card.querySelector("#Children");
-        let protrayedBy = card.querySelector("#ProtrayedBy");
-        let DOB = card.querySelector("#DOB");
+        let img = Car.querySelector("img");
+        let name = Car.querySelector(".carousel-caption h5");
+        let house = Car.querySelector("#House");
+        let children = Car.querySelector("#Children");
+        let protrayedBy = Car.querySelector("#ProtrayedBy");
+        let DOB = Car.querySelector("#DOB");
         img.src = data[randomNumber].image;
+        console.log(data[randomNumber].image)
         img.alt = data[randomNumber].fullName;
         DOB.innerText = `- ${data[randomNumber].birthdate}`;
         name.innerHTML = data[randomNumber].fullName;
         house.innerText = `Belongs to ${data[randomNumber].hogwartsHouse}`;
 
-        if(data[randomNumber].children && data[randomNumber].children.length > 0){
+        if (data[randomNumber].children && data[randomNumber].children.length > 0) {
             let children_names = data[randomNumber].children.join(", ");
             children.innerText = `Children.. ${children_names}`;
         }
-        else{
+        else {
             children.innerText = "Data of this Character's Child is not Available";
         }
         protrayedBy.innerText = `Portrayed by ${data[randomNumber].interpretedBy}`
     })
+    document.body.style.visibility = "visible";
 }
 
 let shuffle = document.querySelector(".btn");
@@ -42,3 +43,10 @@ shuffle.addEventListener("click",()=>{
 });
 
 window.onload = getdata;
+// window.addEventListener("load", getdata);
+// document.addEventListener("DOMContentLoaded", getdata);
+
+// window.onload = () => {
+//     getdata();
+//     document.body.style.visibility = "visible";
+// };
